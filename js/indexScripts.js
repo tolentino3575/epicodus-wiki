e8d4cd0ec27b03373b404849f76c57ed1d905d36
Brad Pizzimenti and Polina Nenchev <brad.pizzimenti@gmail.com>
2016-01-14 10:07:52 -0800

    create index html page with six coding terms and hoverable definitions

---
+++ js/indexScripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $(".markdown-click").hover(function() {
+    $(".markdown-hide").toggle();
+    $(".markdown-show").toggle();
+  });
+  $(".elements-click").hover(function() {
+    $(".elements-hide").toggle();
+    $(".elements-show").toggle();
+  });
+  $(".htmlcss-click").hover(function() {
+    $(".htmlcss-hide").toggle();
+    $(".htmlcss-show").toggle();
+  });
+  $(".jsattributes-click").hover(function() {
+    $(".jsattributes-hide").toggle();
+    $(".jsattributes-show").toggle();
+  });
+  $(".dom-click").hover(function() {
+    $(".dom-hide").toggle();
+    $(".dom-show").toggle();
+  });
+  $(".forms-click").hover(function() {
+    $(".forms-hide").toggle();
+    $(".forms-show").toggle();
+  });
+});
