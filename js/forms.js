e70b82febed3391b17951cb34e7c7f7a5431e2b1
Brad Pizzimenti and Polina Nenchev <brad.pizzimenti@gmail.com>
2016-01-14 14:39:12 -0800

    update forms and create javascript to make form display names

---
+++ js/forms.js
@@ -0,0 +1,14 @@
+$(function () {
+  $("#nameForm").submit(function(event) {
+    var firstNameInput = $("input#firstName").val();
+    var lastNameInput = $("input#lastName").val();
+    var fullname = firstNameInput+lastNameInput;
+// debugger;
+    // $("#firstNameText").text(firstNameInput);
+    // $("#lastNameText").text(lastNameInput);
+
+    $("#fullname").text(fullname);
+    event.preventDefault();
+
+  });
+});
