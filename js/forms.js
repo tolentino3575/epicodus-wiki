95ff3926591d1c9d87c0e7017d8d6557d47e2676
Brad Pizzimenti and Polina Nenchev <polina.mihova@yahoo.com>
2016-01-14 16:18:26 -0800

    refactor index.html with new divs and new classes and add hover

--- js/forms.js
+++ js/forms.js
@@ -2,7 +2,7 @@ $(function () {
   $("#nameForm").submit(function(event) {
     var firstNameInput = $("input#firstName").val();
     var lastNameInput = $("input#lastName").val();
-    var fullname = firstNameInput+lastNameInput;
+    var fullname = firstNameInput + " " + lastNameInput;
 // debugger;
     // $("#firstNameText").text(firstNameInput);
     // $("#lastNameText").text(lastNameInput);
